c7b78984b3f80397d49d5afe4517510b50f19615
wushengju <1205902694@qq.com>
2018-09-11 20:11:10 +0800

    新增多线程学习示例

---
+++ src/main/java/multithread/ThreadTest.java
@@ -0,0 +1,12 @@
+package multithread;
+
+/**
+ * 继承Thread测试类
+ *
+ * @author Sunny
+ * @version 1.0
+ * @className ThreadTest
+ * @date 2018/9/11 下午8:09
+ */
+public class ThreadTest extends Thread {
+}
